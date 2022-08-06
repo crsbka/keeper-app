daa86f24bed10e3657f42a58d2703f0b25604c94
Kristina <crsbka@bk.ru>
2022-08-06 14:34:00 -0400

    Change hello phrase

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello Kasper!</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello Kasperr!</h1>, document.getElementById('root'));
 
