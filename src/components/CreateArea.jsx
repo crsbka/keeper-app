0e77fb27106482652904151e2d3acd3ded2afcf8
Kristina <crsbka@bk.ru>
2022-08-26 18:22:28 -0400

    New content

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+    const [titleText, setTitleText] = useState("");
+    const [contentText, setContentText] = useState("");
+
+
+    function handleTitleChange(event) {
+        const newTitleValue = event.target.value
+        setTitleText(newTitleValue)
+    }
+
+    function handleContentChange(event) {
+        const newContentValue = event.target.value
+        setContentText(newContentValue)
+    }
+
+
+    function addNote() {
+        alert(`You added ${titleText} and ${contentText}`)
+    }
+
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
-                <button>Add</button>
+                <input name="title"
+                       placeholder="Title"
+                       onChange={handleTitleChange}
+                       value={titleText}
+                />
+                <textarea
+                    name="content"
+                    placeholder="Take a note..."
+                    rows="3"
+                    onChange={handleContentChange}
+                    value={contentText}
+                />
+                <button
+                    onClick={addNote}>
+                    <span>+</span>
+                </button>
             </form>
         </div>
     );
