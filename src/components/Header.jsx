087e51dc21cc1af6e8bf1041b574fa8ff2f1b800
Kristina <crsbka@bk.ru>
2022-08-30 12:02:21 -0400

    Revised style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import BubbleChartIcon from '@mui/icons-material/BubbleChart';
 
 function Header() {
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1>
+                <BubbleChartIcon/>
+                Keeper</h1>
         </header>
     );
 }
